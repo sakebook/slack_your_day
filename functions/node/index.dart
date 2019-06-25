10ad913eab1f194ec8b80cbf7976d9264e4f4419
sakebook <sakebook@gmail.com>
2019-06-25 21:09:50 +0900

    Refactor multi user

--- functions/node/index.dart
+++ functions/node/index.dart
@@ -4,6 +4,8 @@ import 'package:node_http/node_http.dart' as http;
 
 import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 
+const EMOJIS = [":one:", ":two:", ":three:"];
+
 void main() {
   functions['yourDay'] = functions.https.onRequest(yourDay);
 }
@@ -13,34 +15,39 @@ void yourDay(ExpressHttpRequest request) async {
   final config = functions.config;
   final token = config.get("secret.token");
   if (token == null) {
-    request.response.statusCode = 400;
-    request.response.headers.add("Content-type", "application/json");
-    request.response.writeln(_createJSON("Not set Token"));
-    request.response.close();
+    _responseComplete(request, 400, "Not set Token");
     return;
   }
   final Map<String, dynamic> jsonBody = request.body;
   final text = jsonBody["text"] as String;
-  RegExp exp = RegExp("(?<=\\^)([^|]*|)");
-  if (!exp.hasMatch(text)) {
-    request.response.statusCode = 400;
-    request.response.headers.add("Content-type", "application/json");
-    request.response.writeln(_createJSON("Group not found"));
-    request.response.close();
+  final userGroupId = _parseGroupId(text);
+  if (userGroupId == null) {
+    _responseComplete(request, 400, "Group not found");
     return;
   }
-  final match = exp.firstMatch(text);
-  final userGroupId = match.group(0);
   final users = await _getUsers(token, userGroupId);
   print("userGroupId: $userGroupId");
   print("users: $users");
-  final user = _choiceUser(users);
-  print("choiceUser: $user");
+  final postText = _postTextFromUsers(users, EMOJIS.length);
+  _responseComplete(request, 200, postText);
+}
 
-  request.response.statusCode = 200;
+
+void _responseComplete(ExpressHttpRequest request, int statusCode, String text) {
+  request.response.statusCode = statusCode;
   request.response.headers.add("Content-type", "application/json");
-  request.response.writeln(_createJSON("本日の主役 <@$user>"));
+  request.response.writeln(_createJSON(text));
   request.response.close();
+}
+
+String _parseGroupId(String text) {
+  RegExp exp = RegExp("(?<=\\^)([^|]*|)");
+  if (!exp.hasMatch(text)) {
+    return null;
+  }
+  final match = exp.firstMatch(text);
+  final userGroupId = match.group(0);
+  return userGroupId;
 }
 
 
@@ -56,7 +63,13 @@ Future<List<dynamic>> _getUsers(String token, String usergroupId) async {
   return jsonBody["users"] as List<dynamic>;
 }
 
-dynamic _choiceUser(List<dynamic> users) {
+String _postTextFromUsers(List<dynamic> users, int limit) {
   users.shuffle(Random());
-  return users.first;
+  String text = "本日の主役 ";
+  final length = (users.length < limit) ? users.length : limit;
+  for (int i = 0; i < length; i++) {
+    text += "${EMOJIS[i]} <@${users[i]}> ";
+  }
+  return text;
 }
+
